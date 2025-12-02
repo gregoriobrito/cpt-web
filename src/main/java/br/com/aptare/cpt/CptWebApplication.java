b41815d52595ee6a5532b707d5cb93baf74a3d9d
Gregorio Brito <gregorio@aptaretecnologia.com.br>
2025-12-02 16:47:56 -0300

    Initial commit

---
+++ src/main/java/br/com/aptare/cpt/CptWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.aptare.cpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CptWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CptWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aptare/cpt/CptWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aptare.cpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CptWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
