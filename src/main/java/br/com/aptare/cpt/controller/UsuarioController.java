6d5a82aa5c8a740c903510c7146920857123eaf1
Gregorio Brito <gregorio@aptaretecnologia.com.br>
2026-01-09 18:07:11 -0300

    melhorias mensagem

--- src/main/java/br/com/aptare/cpt/controller/UsuarioController.java
+++ src/main/java/br/com/aptare/cpt/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import br.com.aptare.cpt.request.UsuarioCadastroRequest;
 import br.com.aptare.cpt.request.VincularRachaRequest;
 import br.com.aptare.cpt.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import br.com.aptare.cpt.repository.UsuarioRepository;
 import br.com.aptare.cpt.security.UserPrincipal;
@@ -16,6 +17,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/usuario")
@@ -38,7 +40,7 @@ public class UsuarioController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         UserPrincipal user = (UserPrincipal) auth.getPrincipal();
 
-        Usuario retorno = usuarioRepository.findById(user.getId()).orElseThrow(() -> new RuntimeException("Usuário não encontrada"));
+        Usuario retorno = usuarioRepository.findById(user.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNPROCESSABLE_CONTENT, "Usuário não encontrada"));
         retorno.setSenha(null);
         return retorno;
     }
@@ -50,7 +52,8 @@ public class UsuarioController {
         RachaUsuario rachaUsuario = rachaUsuarioRepository.findByRachaUsuario(idRacha, retorno.getCodigo());
 
         if (rachaUsuario != null) {
-            throw new RuntimeException("Este usuário já está vinculado a esse racha");
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_CONTENT,
+                    "Este usuário já está vinculado a esse racha");
         }
 
         retorno.setSenha(null);
--- src/main/java/br/com/aptare/cpt/security/SecurityConfig.java
+++ src/main/java/br/com/aptare/cpt/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/error").permitAll()
                         .requestMatchers("/login/**").permitAll()
                         .requestMatchers("/racha/**").permitAll()
                         .requestMatchers("/partida/**").permitAll()
